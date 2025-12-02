fb828eb33f236f29a52fec0a46a31960352a7e13
Simone Elizabeth Gardner <simonegdccc@gmail.com>
2025-12-02 03:44:13 -0500

    Implemented exercise 3

--- world.php
+++ world.php
@@ -5,11 +5,17 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$country = '';
+if (array_key_exists('country', $_GET)) {
+    $country = $_GET['country'];
+}
 
+$query = "SELECT * FROM countries WHERE name LIKE '%$country%'";
+
+$results = $conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
