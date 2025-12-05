c5f86e6be6ddf46331b77cf32d43f987bd027069
Simone Elizabeth Gardner <simonegdccc@gmail.com>
2025-12-05 03:38:27 -0500

    implemented exercise 5

--- world.js
+++ world.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const lookupButton=document.getElementById('lookup');
     const rDiv=document.getElementById('result');
     const country=document.getElementById('country');
+    const citiesButton=document.getElementById('cities');
 
 
     lookupButton.addEventListener('click', function() {
@@ -13,13 +14,28 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch(url)
 
             .then(aj_resp => aj_resp.text())
-            .then(html_results => {
-                rDiv.innerHTML = html_results;
+            .then(html_results=> {
+                rDiv.innerHTML =html_results;
             })
             .catch(e=>{ //just to catch in case there's an error
                 rDiv.innerHTML = "<p>Error: Unable to load data!</p>";
                 console.error(e);
             });
 
+    });
+
+    citiesButton.addEventListener('click', function() {
+        const countryValue=country.value;
+        const url = `cities.php?country=${encodeURIComponent(countryValue)}`;
+
+        fetch(url)
+        .then(aj_resp => aj_resp.text())
+        .then(html_results => {
+            rDiv.innerHTML= html_results;
+        })
+        .catch (e=>{  //again, just to catch in case there's an error
+            rDiv.innerHTML = "<p>Error: Unable to load data!</p>";
+            console.error(e);
+        });
     });
 });
--- world.php
+++ world.php
@@ -11,27 +11,64 @@ if (array_key_exists('country', $_GET)) {
     $country = $_GET['country'];
 }
 
-$query = "SELECT * FROM countries WHERE name LIKE '%$country%'";
+$is_city_lookup = false;
+if (array_key_exists('lookup', $_GET) && $_GET['lookup'] === 'cities') {
+    $is_city_lookup = true;
+}
 
-$countries = $conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
+if ($is_city_lookup) {
+    $query = "SELECT cities.name AS city_name, cities.district, cities.population
+              FROM cities
+              JOIN countries ON cities.country_code = countries.code
+              WHERE countries.name LIKE '%$country%'";
+
+    $cities = $conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>District</th>
+        <th>Population</th>
+      </tr>
+
+    <?php foreach($cities as $i): ?>
+      <tr>
+        <td><?=$i['city_name'] ?></td>
+        <td><?=$i['district'] ?></td>
+        <td><?=$i['population'] ?></td>
+      </tr>
+    <?php endforeach; 
+    ?>
+    </table>
+
+    <?php
+} else {
+
+    $query = "SELECT * FROM countries WHERE name LIKE '%$country%'";
+
+    $countries = $conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+
+    <table>
+      <tr>
+        <th>Country Name</th>
+        <th>Continent</th>
+        <th>Independence Year</th>
+        <th>Head of State</th>
+      </tr>
+
+
+    <?php foreach($countries as $i): ?>
+      <tr>
+        <td><?=$i['name'] ?></td>
+        <td><?=$i['continent'] ?></td>
+        <td><?=$i['independence_year'] ?></td>
+        <td><?=$i['head_of_state'] ?></td>
+      </tr>
+    <?php endforeach; 
+    ?>
+    </table>
+    <?php
+}
 ?>
-
-<table>
-  <tr>
-    <th>Country Name</th>
-    <th>Continent</th>
-    <th>Independence Year</th>
-    <th>Head of State</th>
-  </tr>
-
-
-<?php foreach($countries as $i): ?>
-  <tr>
-    <td><?=$i['name'] ?></td>
-    <td><?=$i['continent'] ?></td>
-    <td><?=$i['independence_year'] ?></td>
-    <td><?=$i['head_of_state'] ?></td>
-  </tr>
-<?php endforeach; 
-?>
-</table>
